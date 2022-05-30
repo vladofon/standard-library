78e0cb9095b9b475d1c8cbc9e9c6fb416a44cb38
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-05-30 21:36:36 +0300

    Add io support

--- standard-library/Task2.h
+++ standard-library/Task2.h
@@ -1,6 +1,99 @@
 #pragma once
+#include <algorithm>
+#include <deque>
+#include <iostream>
+#include <stack>
+
+#include "Employee.h"
+#include "IOController.h"
+#include "LinkedList.h"
+
+using namespace std;
 
 class task2
 {
+public:
 
+   static void start()
+   {
+      const auto container = new deque<string>();
+
+      fill(container);
+
+      review(container);
+
+      container->clear();
+
+      fill(container);
+
+      review(container);
+
+      const auto second_container = new deque<string>();
+
+      fill(second_container);
+
+      erase_range(container, 0, 5);
+
+      sort(container->begin(), container->end());
+      sort(second_container->begin(), second_container->end());
+
+      merge(
+         container->begin(),
+         container->end(),
+         second_container->begin(),
+         second_container->end(),
+         inserter(*container, container->end())
+      );
+
+      review(container);
+   }
+
+private:
+
+   static void fill(deque<string>* container)
+   {
+      const auto employees = new LinkedList<string>();
+      employees->add((new employee("first", 1, 1))->to_string());
+      employees->add((new employee("second", 2, 2))->to_string());
+      employees->add((new employee("third", 3, 3))->to_string());
+
+      io_controller::save(employees, "db.txt");
+
+      for (int i = 0; i < employees->getSize(); i++)
+      {
+         container->push_front(employees->get(i));
+      }
+   }
+
+   // Print container using iterator
+   static void review(const deque<string>* container)
+   {
+      cout << "First container:" << endl;
+      for (auto item = container->begin(); item != container->end(); ++item)
+      {
+         cout << "[item] = " << *item << endl;
+      }
+   }
+
+   static void erase_range(deque<string>* container, const int start, const int end)
+   {
+      auto first_pos = find_pos(container, start);
+      auto second_pos = find_pos(container, end);
+
+      container->erase(first_pos, second_pos);
+   }
+
+   static deque<string>::iterator find_pos(deque<string>* container, const int index)
+   {
+      auto position = container->begin();
+
+      int counter = 0;
+      while (counter != index)
+      {
+         position++;
+         counter++;
+      }
+
+      return position;
+   }
 };
